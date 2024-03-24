e5065079544211fcf9eecf0519fb4f210a490f45
Gregory Daskalakis <dask.gregory@gmail.com>
2024-03-24 02:03:15 +0200

    #4 Color Picker, File Upload, Tshirt Camera

---
+++ src/canvas/CameraRig.jsx
@@ -0,0 +1,40 @@
+import React, {useRef} from 'react'
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+
+function CameraRig( {children }) {
+    const group = useRef();
+    const snap = useSnapshot(state);
+    //allows you to execute code to any frame
+    useFrame((state, delta)=>{
+        const isBreakingPoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <= 600;
+
+        // set the initial position of the model
+        let targetPosition = [-0.4, 0,2];
+        if(snap.intro){
+            if(isBreakingPoint) targetPosition = [0,0,2];
+            if(isMobile) targetPosition = [0,0.2,2.5]
+        }else{
+            if(isMobile) targetPosition =  [0,0,2.5];
+            else targetPosition = [0,0,2];
+        }
+        // set camera position 
+        easing.damp3(state.camera.position, targetPosition,0.25,delta);
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y / 10, -state.pointer.x / 5, 0],
+            0.25,
+            delta
+        )
+    })
+    // see the model rotation smoothly
+    
+  return <group ref={group}>{children}</group>
+}
+
+export default CameraRig
